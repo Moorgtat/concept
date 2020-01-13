7bd5b0532edc642f58f1390c3e2bca80ae5b8b2a
Moorgtat <mat.lecardinal@gmail.com>
2020-01-13 18:21:06 +0100

    Html mis en place div.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,55 @@
 <template>
   <div id="Home">
-    <h1>Home</h1>
+
+      <div id="navbar-container">
+          <div id="logo">
+              <h1>Logo</h1>
+          </div>
+          <div id="menu">
+              <h1>Navbar</h1>
+              <ul>
+                  <li>Accueil</li>
+                  <li>Menu</li>
+                  <li>Nous contacter</li>
+              </ul>
+          </div>
+      </div>
+
+      <div id="carroussel-container">
+          <h1>Carroussel Top</h1>
+      </div>
+
+      <div id="presentation-container">
+          <h1>Texte</h1>
+      </div>
+
+      <div id="bandeau-3p-container">
+          <h1>Photos-Trois</h1>
+      </div>
+
+      <div id="bandeau-2p1-container">
+          <h1>Photos-Deux</h1>
+      </div>
+
+      <div id="bandeau-2p2-container">
+          <h1>Photos-Deux</h1>
+      </div>
+
+      <div id="explication-container">
+          <h1>Texte</h1>
+      </div>
+
+      <div id="carroussel-avis-container">
+          <h1>Carroussel Avis</h1>
+      </div>
+
+          <div id="footer-container">
+              <h1>Footer</h1>
+              <h1>Adresse</h1>
+              <h1>Divers</h1>
+          </div>
+
+
   </div>
 </template>
 
@@ -12,9 +61,49 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#Home{
-  border: solid 2px black;
-  width: 100vm;
-  height: 100vh;
-}
+    #Home {
+        border: solid 2px black;
+        width: 100%;
+    }
+
+    #navbar-container {
+        border: solid 2px black;
+    
+    }
+
+    #carroussel-container {
+        border: solid 2px black;
+        width: 100%;
+        height: 100vh;
+        background-color: darkgrey;
+    }
+
+    #presentation-container {
+
+    }
+
+    #bandeau-3p-container {
+
+    }
+
+    #bandeau-2p1-container {
+
+    }
+
+    #bandeau-2p2-container {
+
+    }
+
+    #explication-container {
+
+    }
+
+    #carroussel-avis-container {
+
+    }
+
+    #footer-container {
+
+    }
+
 </style>
